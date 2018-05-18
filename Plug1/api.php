f75d37dd594c72bf0f11f7b5ca33810171c0a9b2
Zip Code <featurefirm@gmail.com>
2018-05-18 23:32:07 +1000

    Update api.php

--- Plug1/api.php
+++ Plug1/api.php
@@ -1,11 +1,8 @@
 <?php
 
-//download TAR
-
-<?php
 
 
-//download Tar
+//download and get Tar
 $tar = 'somefile.tar';
 $tar_path = '/the/full/path/to/where/the/file/is' . $tar;
 $size = filesize($tar_path);
@@ -17,32 +14,10 @@ header("Content-Transfer-Encoding: binary");
 
 readfile($tar_path);
 
+//save .tar to external DST
+
 //conect to the db
 mysql_connect("4ten.powwebmysql.com","root",'pass_root') or die (mysql_error)
-mysql_select_db("users") or die (mysql_error());
-
-//call passed fann_get_activation_function
-  if(function_exists($_GET['method'])) {
-    $_GET['method']();
-  }
-}
-
-//method_exists
-
-function getALLUsers() {
-$user_sql = mysql_query("select * from user"
-$users = array ();
-while($user = mysql_fetch_array($user_sql)) {
-  $users[] = $user;
-}
-$users = json_encode($users);
-echo $_GET['jsoncallback'] . '(' . $users . ')'
-}
-?>
-
-
-//conect to the db
-mysql_connect("connect to docker ucrm db ","root",'pass_root') or die (mysql_error)
 mysql_select_db("users") or die (mysql_error());
 
 //call passed fann_get_activation_function
