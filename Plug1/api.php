69b9395341ce87eb66e9d1925eeb66e9adcaa3e7
Zip Code <featurefirm@gmail.com>
2018-05-18 23:15:30 +1000

    Create api.php

---
+++ Plug1/api.php
@@ -0,0 +1,24 @@
+<?php
+
+//conect to the db
+mysql_connect("4ten.powwebmysql.com","root",'pass_root') or die (mysql_error)
+mysql_select_db("users") or die (mysql_error());
+
+//call passed fann_get_activation_function
+  if(function_exists($_GET['method'])) {
+    $_GET['method']();
+  }
+}
+
+//method_exists
+
+function getALLUsers() {
+$user_sql = mysql_query("select * from user"
+$users = array ();
+while($user = mysql_fetch_array($user_sql)) {
+  $users[] = $user;
+}
+$users = json_encode($users);
+echo $_GET['jsoncallback'] . '(' . $users . ')'
+}
+?>
